57fe31699913163c26697f679543e4927b9c4e2b
Jesus Hernandez <jehrivas@gmail.com>
2017-10-25 10:12:21 -0600

    configurando el buscador

--- angular-projects/portafolio/src/app/components/index.paginas.ts
+++ angular-projects/portafolio/src/app/components/index.paginas.ts
@@ -2,3 +2,4 @@
 export { PortafolioComponent } from './portafolio/portafolio.component';
 export { AboutComponent } from './about/about.component';
 export { ProductoComponent  } from './producto/producto.component';
+export { SearchComponent } from './search/search.component';
