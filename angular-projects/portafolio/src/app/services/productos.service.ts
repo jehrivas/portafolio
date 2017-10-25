f75db0e059674309acc55384373d235b34da9933
Jesus Hernandez <jehrivas@gmail.com>
2017-10-25 13:58:41 -0600

    completa

--- angular-projects/portafolio/src/app/services/productos.service.ts
+++ angular-projects/portafolio/src/app/services/productos.service.ts
@@ -5,12 +5,48 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos: any [] = [];
+  productos_filtrado: any[] = [];
   // tslint:disable-next-line:no-inferrable-types
   cargando: boolean = true;
 
   constructor( private http: Http ) {
 
     this.cargar_productos();
+
+   }
+
+   public buscar_producto( termino: string ) {
+
+
+    if ( this.productos.length === 0) {
+      this.cargar_productos().then( () => {
+
+        // termino la carga.
+        this.filtrar_productos(termino);
+      });
+
+      } else {
+      this.filtrar_productos(termino);
+    }
+
+
+   }
+
+   private filtrar_productos(termino: string) {
+
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      if ( prod.categoria.indexOf (termino ) >= 0 || prod.titulo.toLowerCase().indexOf( termino ) >= 0 ) {
+        this.productos_filtrado.push ( prod );
+       // console.log ( prod );
+      }
+      // console.log( prod );
+
+    });
 
    }
 
@@ -22,13 +58,23 @@ public cargar_productos() {
 
   this.cargando = true;
 
-   this.http.get('https://paginaweb-94813.firebaseio.com/productos_idx.json').subscribe( res => {
+  // tslint:disable-next-line:prefer-const
+  let promesa = new Promise ( ( resolve, reject ) => {
 
-    // console.log(res.json());
-    this.cargando = false;
-    this.productos = res.json();
+    this.http.get('https://paginaweb-94813.firebaseio.com/productos_idx.json').subscribe( res => {
 
-    });
+          // console.log(res.json());
+          this.cargando = false;
+          this.productos = res.json();
+          resolve();
+
+          });
+
+  });
+
+  return promesa;
+
+
 
   }
 
